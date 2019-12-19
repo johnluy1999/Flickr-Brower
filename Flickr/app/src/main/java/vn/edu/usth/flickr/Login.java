7bac020fe9ab3173464175746e06063a46a81f02
Nguyen Tuan Duy <johnluy1999@gmail.com>
2019-12-19 11:14:43 +0700

    Login

---
+++ Flickr/app/src/main/java/vn/edu/usth/flickr/Login.java
@@ -0,0 +1,81 @@
+package vn.edu.usth.flickr;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.nfc.Tag;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class Login extends AppCompatActivity {
+
+    private EditText username;
+    private EditText password;
+    private TextView register , warning;
+    private Button login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        username = (EditText) findViewById(R.id.username);
+        password = (EditText) findViewById(R.id.password);
+        register = (TextView) findViewById(R.id.register);
+        login    = (Button)   findViewById(R.id.loginbutton);
+        warning  = (TextView) findViewById(R.id.warning);
+
+
+        username.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                username.setText("");
+                return false;
+            }
+        });
+
+
+        password.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                password.setText("");
+                return false;
+            }
+        });
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                validate(username.getText().toString(),password.getText().toString());
+            }
+        });
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //chuyen sang register frame
+            }
+        });
+    }
+
+    //----------------------------------------------------------------------------------------------
+    private void validate (String username, String password){
+
+        if ( (username.equals("admin")) && (password.equals("admin"))){
+            //chuyen sang main acctivity
+            Intent intent = new Intent(Login.this,MainActivity.class);
+            startActivity(intent);
+        }
+        else{
+            warning.setText("Invalid Username or Password !");
+            warning.setVisibility(View.VISIBLE);
+        }
+    }
+
+}
